898b0b227090f56e4e576e02abeccbaf3e64fe78
alexandrov.ua <alexandrov.ua@gmail.com>
2024-08-06 20:09:43 +0300

    read file

--- app/main.go
+++ app/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
+	var dirName = flag.String("directory", "/tmp/", "Dirictory name")
 	fmt.Println("Logs from your program will appear here!")
 	srv := CreateServer()
 
@@ -14,6 +17,13 @@ func main() {
 	})
 	srv.Get("/user-agent", func(r RequestContext) {
 		r.RespondWithStatusString(200, r.GetHeader("User-Agent"))
+	})
+	srv.Get("/files/{fileName}", func(r RequestContext) {
+		if fileText, err := os.ReadFile(*dirName + r.GetParam("fileName")); err == nil {
+			r.RespondWithStatusString(200, string(fileText))
+		} else {
+			r.RespondWithStatusString(404, "File doesn't exists")
+		}
 	})
 
 	srv.Listen("0.0.0.0:4221")
