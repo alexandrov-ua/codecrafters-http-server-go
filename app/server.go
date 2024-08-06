1204083934f20bc882381c5a5b886a934e9b4c83
alexandrov.ua <alexandrov.ua@gmail.com>
2024-08-06 16:07:55 +0300

    Extract URL path

--- app/server.go
+++ app/server.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"errors"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
+var urls = make(map[string]int)
+
 func main() {
+	urls["/"] = 1
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -16,9 +23,46 @@ func main() {
 	}
 
 	conn, err := l.Accept()
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if err := AcceptConnection(conn); err != nil {
+		fmt.Println(err)
+		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+	}
+}
+
+func AcceptConnection(conn net.Conn) error {
+
+	buff := make([]byte, 1024)
+	n, err := conn.Read(buff)
+	if err != nil {
+		return fmt.Errorf("error accepting the connection: %w", err)
+	}
+	reader := bufio.NewReader(bytes.NewReader(buff[:n]))
+	line, _, err := reader.ReadLine()
+	if err != nil {
+		return fmt.Errorf("error accepting the connection: %w", err)
+	}
+	_, path, err := ParseStartLine(line)
+	if err != nil {
+		return fmt.Errorf("error accepting the connection: %w", err)
+	}
+
+	if _, ok := urls[path]; ok {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+	return nil
+}
+
+func ParseStartLine(l []byte) (verb string, path string, err error) {
+	arr := strings.Split(string(l), " ")
+	if len(arr) != 3 || (arr[2] != "HTTP/1.1" && arr[2] != "HTTP/1.0") {
+		return "", "", errors.New("not a http")
+	}
+	return arr[0], arr[1], nil
 }
